7c8caec5606204f7d78bc4836169f6b77048de55
Endorocket <endorocket@gmail.com>
2023-01-08 02:25:01 +0100

    add @CrossOrigin

--- src/main/java/eu/horyzont/neighbourssocializer/infrastructure/rest/event/EventRestController.java
+++ src/main/java/eu/horyzont/neighbourssocializer/infrastructure/rest/event/EventRestController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
+@CrossOrigin
 public class EventRestController {
 
     private final EventApplicationService eventApplicationService;
